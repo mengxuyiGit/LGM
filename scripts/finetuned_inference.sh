538f62e01e6fa8be3d7ecbd97af8aa8f94e736f2
mengxuyiGit <mengxuyiabc@gmail.com>
2024-05-26 20:23:35 +0000

    inference script for single attr unet

--- scripts/finetuned_inference.sh
+++ scripts/finetuned_inference.sh
@@ -58,7 +58,7 @@ accelerate launch --config_file acc_configs/gpu1.yaml main_zero123plus_v5_batch_
     --lr_scheduler_patience 5 --lr_scheduler_factor 0.7 --lr_schedule_by_train \
     --prob_cam_jitter 0 --input_size 320 --num_input_views 6 --num_views 20 \
     --lambda_splatter 1 --lambda_rendering 1 --lambda_alpha 0 --lambda_lpips 1 \
-    --desc 'inference_unet_single_attr_RGBs_run2' --data_path_rendering ${DATA_RENDERING_ROOT_LVIS_46K_CLUSTER} \
+    --desc 'inference_save_cond_unet_single_attr_RGBs_seed123' --data_path_rendering ${DATA_RENDERING_ROOT_LVIS_46K_CLUSTER} \
     --data_path_vae_splatter ${DATA_DIR_BATCH_LVIS_SPLATTERS_MV_ROOT_CLUSTER} \
     --set_random_seed --batch_size 1 --num_workers 2 \
     --skip_predict_x0 --scale_act 'biased_softplus' --scale_act_bias -3 --scale_bias_learnable \
