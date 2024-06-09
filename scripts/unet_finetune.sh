e8e55cfe7e6e1ec905ff419a4d68422e892575b7
mengxuyiGit <mengxuyiabc@gmail.com>
2024-06-09 16:44:44 +0000

    updated script

--- scripts/unet_finetune.sh
+++ scripts/unet_finetune.sh
@@ -102,15 +102,37 @@ DATA_DIR_BATCH_LVIS_SPLATTERS_MV_ROOT=/mnt/lingjie_cache/lvis_splatters/testing
 #     --train_unet --only_train_attention \
 #     --batch_size 1 --num_workers 1 --gradient_accumulation_steps 1 
 
-# [JUN 7] Add rendering loss
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-accelerate launch --main_process_port 29518 --config_file acc_configs/gpu4.yaml main_zero123plus_v5_batch_marigold_finetune_decoder_unet_accumulate_shared.py big \
-    --workspace runs/finetune_unet/workspace_train \
+# # [JUN 7] Add rendering loss
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+# accelerate launch --main_process_port 29518 --config_file acc_configs/gpu4.yaml main_zero123plus_v5_batch_marigold_finetune_decoder_unet_accumulate_shared.py big \
+#     --workspace runs/finetune_unet/workspace_train \
+#     --lr 1e-5 --max_train_steps 30000 --eval_iter 500 --save_iter 1000 --lr_scheduler Plat \
+#     --lr_scheduler_patience 50 --lr_scheduler_factor 0.7 --lr_schedule_by_train \
+#     --prob_cam_jitter 0 --input_size 320 --output_size 320 --num_input_views 6 --num_views 10 \
+#     --lambda_splatter 0 --lambda_rendering 1 --lambda_alpha 0 --lambda_lpips 1  --lambda_splatter_lpips 0 \
+#     --desc 'use_fted_deocder-rendering_mse_lpips_loss-only_train_attention-v7' --data_path_rendering ${DATA_RENDERING_ROOT_LVIS_46K} \
+#     --data_path_vae_splatter ${DATA_DIR_BATCH_LVIS_SPLATTERS_MV_ROOT} \
+#     --set_random_seed --scale_act 'biased_softplus' --scale_act_bias -3 --scale_bias_learnable \
+#     --scale_clamp_max -2 --scale_clamp_min -10 \
+#     --splatter_guidance_interval 1 --save_train_pred -1 --decode_splatter_to_128 \
+#     --decoder_upblocks_interpolate_mode "last_layer" --codes_from_encoder \
+#     --model_type Zero123PlusGaussianMarigoldUnetCrossDomain \
+#     --custom_pipeline "./zero123plus/pipeline_v7_seq.py" --render_input_views --attr_group_mode "v5" \
+#     --bg 1.0 --fovy 50 --rendering_loss_use_weight_t \
+#     --train_unet --only_train_attention \
+#     --batch_size 1 --num_workers 1 --gradient_accumulation_steps 2 \
+#     --resume_decoder /mnt/kostas-graid/sw/envs/xuyimeng/Repo/LGM/runs/finetune_decoder/workspace_train/00007-resume_smallLR_render_lossx10_splatter700steps_4gpus_bsz2_accumulate32-sp_guide_1-codes_from_encoder-v0_unfreeze_all-pred128_last_layer-skip_predict_x0-loss_render10.0_splatter0.1_lpips10.0-lr1e-05-Plat50/eval_global_step_1400_ckpt/model.safetensors 
+    
+
+# [JUN 8] rendering loss seems to have larger scale when inference, compare with no rendering loss to see
+export CUDA_VISIBLE_DEVICES=4,5
+accelerate launch --main_process_port 29518 --config_file acc_configs/gpu1.yaml main_zero123plus_v5_batch_marigold_finetune_decoder_unet_accumulate_shared.py big \
+    --workspace runs/finetune_unet/workspace_debug \
     --lr 1e-5 --max_train_steps 30000 --eval_iter 500 --save_iter 1000 --lr_scheduler Plat \
     --lr_scheduler_patience 50 --lr_scheduler_factor 0.7 --lr_schedule_by_train \
     --prob_cam_jitter 0 --input_size 320 --output_size 320 --num_input_views 6 --num_views 10 \
-    --lambda_splatter 0 --lambda_rendering 1 --lambda_alpha 0 --lambda_lpips 1  --lambda_splatter_lpips 0 \
-    --desc 'rendering_mse_lpips_loss-only_train_attention-v7' --data_path_rendering ${DATA_RENDERING_ROOT_LVIS_46K} \
+    --lambda_splatter 0 --lambda_rendering 0 --lambda_alpha 0 --lambda_lpips 0  --lambda_splatter_lpips 0 \
+    --desc 'NO-rendering_mse_lpips_loss-only_train_attention-v7' --data_path_rendering ${DATA_RENDERING_ROOT_LVIS_46K} \
     --data_path_vae_splatter ${DATA_DIR_BATCH_LVIS_SPLATTERS_MV_ROOT} \
     --set_random_seed --scale_act 'biased_softplus' --scale_act_bias -3 --scale_bias_learnable \
     --scale_clamp_max -2 --scale_clamp_min -10 \
@@ -120,8 +142,9 @@ accelerate launch --main_process_port 29518 --config_file acc_configs/gpu4.yaml 
     --custom_pipeline "./zero123plus/pipeline_v7_seq.py" --render_input_views --attr_group_mode "v5" \
     --bg 1.0 --fovy 50 --rendering_loss_use_weight_t \
     --train_unet --only_train_attention \
-    --batch_size 1 --num_workers 1 --gradient_accumulation_steps 2 
-    
+    --batch_size 1 --num_workers 1 --gradient_accumulation_steps 2 \
+    --resume_unet /mnt/kostas-graid/sw/envs/xuyimeng/Repo/LGM/runs/finetune_unet/workspace_train/20240556-no-rendering_loss-only_train_attention-v7-log_dirty_data-sp_guide_1-codes_from_encoder-v0_unfreeze_all-pred128_last_layer-train_unet-numV10-loss_render1.0_lpips1.0-lr1e-05-Plat50/eval_global_step_2800_ckpt/model.safetensors
+
 # [DEBUG] 
 DATA_RENDERING_ROOT_LVIS_46K_CLUSTER=/home/chenwang/data/lvis_dataset/testing
 DATA_DIR_BATCH_LVIS_SPLATTERS_MV_ROOT_CLUSTER=/mnt/kostas-graid/sw/envs/xuyimeng/Data/lvis/data_processing/testing
