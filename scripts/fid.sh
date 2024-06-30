d043235ae85bc82721fcba9db46f6c0b1efb012a
mengxuyiGit <mengxuyiabc@gmail.com>
2024-06-30 23:49:29 +0000

    inference on unet trained with rendering loss / finetune all layers

---
+++ scripts/fid.sh
@@ -0,0 +1,7 @@
+python -m pytorch_fid --save-stats path/to/dataset path/to/outputfile
+
+python -m pytorch_fid path/to/dataset1 path/to/dataset2
+
+# real
+/home/chenwang/data/lvis_dataset/testing_subset/fid50k_2500obj20v_stats.npz
+# /home/chenwang/data/lvis_dataset/testing/1000-1999/fid_stats.npz
