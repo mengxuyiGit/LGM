60b1ba20fde8994218de50dd8400eccaafdd8114
mengxuyiGit <mengxuyiabc@gmail.com>
2024-07-15 18:17:21 +0000

    resume unet training

--- scripts/unet_finetune.sh
+++ scripts/unet_finetune.sh
@@ -4,13 +4,13 @@ DATA_DIR_BATCH_LVIS_SPLATTERS_MV_ROOT=/mnt/kostas-graid/datasets/xuyimeng/lvis/d
 # DATA_DIR_BATCH_LVIS_SPLATTERS_MV_ROOT_CLUSTER=/mnt/kostas-graid/datasets/xuyimeng/lvis/data_processing_finetuned_lgm_fov60_8epochs/testing
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-accelerate launch --main_process_port 29518 --config_file acc_configs/gpu4.yaml main_zero123plus_v5_batch_marigold_finetune_decoder_unet_accumulate_shared.py big \
+accelerate launch --main_process_port 29519 --config_file acc_configs/gpu4.yaml main_zero123plus_v5_batch_marigold_finetune_decoder_unet_accumulate_shared.py big \
     --workspace runs/finetune_unet/workspace_train_july \
     --lr 4e-6 --max_train_steps 100000 --eval_iter 1000 --save_iter 1000 --lr_scheduler Plat \
     --lr_scheduler_patience 50 --lr_scheduler_factor 0.7 --lr_schedule_by_train \
     --prob_cam_jitter 0 --input_size 320 --output_size 320 --num_input_views 6 --num_views 10 \
     --lambda_splatter 0 --lambda_rendering 0 --lambda_alpha 0 --lambda_lpips 0 --lambda_splatter_lpips 0 \
-    --desc 'train_unet-sd_encoder-fted_LGM_fov60-resume00007_2k' --data_path_rendering ${DATA_RENDERING_ROOT_LVIS_46K} \
+    --desc 'train_unet-sd_encoder-fted_LGM_fov60-resume00008_total13k' --data_path_rendering ${DATA_RENDERING_ROOT_LVIS_46K} \
     --data_path_vae_splatter ${DATA_DIR_BATCH_LVIS_SPLATTERS_MV_ROOT} \
     --set_random_seed \
     --model_type Zero123PlusGaussianMarigoldUnetCrossDomain \
@@ -19,7 +19,7 @@ accelerate launch --main_process_port 29518 --config_file acc_configs/gpu4.yaml 
     --train_unet --class_emb_cat  --drop_cond_prob 0.1 --only_train_attention \
     --batch_size 2 --num_workers 1 --gradient_accumulation_steps 4 \
     --invalid_list /mnt/kostas_home/lilym/LGM/LGM/data_lists/lvis_invalid_uids_nineviews.json \
-    --resume_unet /mnt/kostas_home/lilym/LGM/LGM/runs/finetune_unet/workspace_train_july/00007-train_unet-sd_encoder-fted_LGM_fov60-NO_resume-train_unet-numV10-loss-lr4e-06-Plat50/eval_global_step_2000_ckpt/model.safetensors \
+    --resume_unet /mnt/kostas_home/lilym/LGM/LGM/runs/finetune_unet/workspace_train_july/00008-train_unet-sd_encoder-fted_LGM_fov60-resume00007_2k-train_unet-numV10-loss-lr4e-06-Plat50/eval_global_step_11000_ckpt/model.safetensors \
     --resume_decoder /mnt/kostas_home/lilym/LGM/LGM/runs/finetune_decoder/workspace_train_july/20240713-sd-decoder-fted_lgm-loss_render1.0_splatter2.0_lpips2.0-lr0.0001-Plat5/eval_global_step_1000_ckpt/model.safetensors
     # note: although the above decoder is used for resume/visualization, it is not the final decoder we are gonna use
 
