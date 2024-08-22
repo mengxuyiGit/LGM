2c412334cd5017000dc53193409a9d96393a56cc
mengxuyiGit <mengxuyiabc@gmail.com>
2024-08-22 22:23:54 +0000

    [training] lara full h5 data of 264k obj, bsz=4, gpus=4

--- scripts/finetune_lgm.sh
+++ scripts/finetune_lgm.sh
@@ -5,14 +5,15 @@ DATA_RENDERING_ROOT_LVIS_46K=/home/chenwang/data/lvis_dataset/testing
 # LARA_h5=/home/xuyimeng/Repo/LaRa/outputs/gobjverse_hdf5_v2_raw_normal_00.hdf5
 LARA_h5=/mnt/kostas-graid/datasets/xuyimeng/GobjLara/dataset/gobjaverse/gobjaverse.h5
 
-CUDA_VISIBLE_DEVICES=0
-accelerate launch --config_file acc_configs/gpu1.yaml main1_lvis.py big --workspace runs/finetune_lgm/workspace_debug \
+CUDA_VISIBLE_DEVICES=1,2,3,4
+# CUDA_VISIBLE_DEVICES=4
+accelerate launch --config_file acc_configs/gpu4.yaml main1_lvis.py big --workspace runs/finetune_lgm/workspace_train_aug \
     --resume pretrained/model_fp16_fixrot.safetensors \
-    --data_path_rendering ${LARA_h5} --data_mode lara --fovy 39.6 --input_size 256 --num_views 12 \
+    --data_path_rendering ${LARA_h5} --data_mode lara --fovy 39.6 --input_size 256 --num_views 10 \
     --prob_cam_jitter 0 \
     --num_input_views 6 \
     --lambda_normal_err 0.0 --lambda_normal 0.2 --lambda_depth 0.5 --normal_depth_begin_iter 5000 --resume_iter 0 \
-    --batch_size 2 --gradient_accumulation_steps 1 --desc "bsz2-lara_dataloader-No_normal_err-splatter_128-normal_loss_0.2_depth_1.0_after500"
+    --batch_size 4 --gradient_accumulation_steps 1 --desc "lara_h5_full-normal0.2_depth0.5_loss_0.5_after5000-no_normal_err"
 
 
 # export CUDA_VISIBLE_DEVICES=0
