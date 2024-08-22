44ac9acf4f7590d2ea618b775df146ec426fdf4b
mengxuyiGit <mengxuyiabc@gmail.com>
2024-08-22 21:37:47 +0000

    support flexible view_id as num_views

--- core/provider_lara.py
+++ core/provider_lara.py
@@ -91,8 +91,8 @@ class gobjverse(torch.utils.data.Dataset):
             view_id = src_view_id + [scene_info['groups'][f'groups_4_{i}'][-1] for i in range(4)]
         
         fixed_input_views = np.arange(25, 37)[::3].tolist() + [2,22] # equals to the original GOBjaverse 27, 30, 33, 36, 2, 22 (because h5 do not include the 25,26 views)
-        view_id = fixed_input_views + np.random.permutation(np.arange(0,38))[:2].tolist()
-        # print("view_id", view_id)
+        view_id = fixed_input_views + np.random.permutation(np.arange(0,38))[:(self.opt.num_views-self.opt.num_input_views)].tolist()
+        print("view_id", len(view_id))
 
         tar_img, bg_colors, tar_nrms, tar_msks, tar_c2ws, tar_w2cs, tar_ixts = self.read_views(scene_info, view_id, scene_name)
 
